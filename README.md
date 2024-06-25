912321f2aac8d3e9d3ed07768bcc88e91db60126
zhuzhen <zhuzhen@ticpsh.com>
2024-06-25 11:09:21 +0800

    py版本

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # ListenShutdow
 局域网内实现使用ios对电脑进行遥控关机
+
+# 原理：
+    通过启动一个服务端口12345，进行监听请求。当接收请求，执行关机命令
+
+# 步骤
+1、安装打包工具
+```
+pip install pyinstaller
+```
+2、打包
+```
+pyinstaller --onefile --noconsole -n shutdown_service index.py
+```
+--onefile：生成单个独立的可执行文件。
+--noconsole：禁用控制台窗口的显示。
+3、设置开机执行
+    有多种方式，比价简单的为：win+R 输入shell:startUp,然后对打包好的exe创建快捷方式，将快捷方式放入到shell:startUp打开的目录下就行。
---
+++ index.py
@@ -0,0 +1,27 @@
+# server.py
+
+import http.server
+import socketserver
+import subprocess
+
+# 监听的端口号
+PORT = 12345
+
+class RequestHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/shutdown':
+            print(f'{PORT}: 收到关机信号，正在关机...')
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            subprocess.run(['shutdown', '/s', '/t', '0'])
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'Not Found')
+
+# 创建 HTTP 服务器并监听指定端口
+with socketserver.TCPServer(("", PORT), RequestHandler) as httpd:
+    print(f'服务器正在监听端口 {PORT}')
+    httpd.serve_forever()
